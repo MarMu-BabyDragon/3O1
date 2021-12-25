08b7afacb241cee880dbb98b5e8d18e411ebc8f4
Baby Dragon <94061352+MarMu-BabyDragon@users.noreply.github.com>
2021-12-25 19:20:34 +0630

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!bin/bash
+
+clear
+echo 
+echo -e """\e[1m\e[35m                                                                  
+          ███╗   ███╗ █████╗ ██████╗ ███╗    ███╗██╗   ██╗               
+          ████╗ ████║██╔══██╗██╔══██╗████╗ ████║██║   ██║              
+          ██╔████╔██║███████║██████╔╝██╔████╔██║██║   ██║              
+          ██║╚██╔╝██║██╔══██║██╔══██╗██║╚██╔╝██║██║   ██║              
+          ██║ ╚═╝ ██║██║  ██║██║  ██║██║ ╚═╝ ██║╚██████╔╝               
+          ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝      ╚═╝ ╚═════╝                
+"""
+echo
+echo -e "\e[34m Do u know MarMu"
+echo"
+apt update
+apt upgrade -y
+apt install figlet -y
+pkg install python -y
+pkg install python2 -y
+pkg install ruby
+pip install tqdm mechanize requests bs4
+pip2 install mechanize request bs4 tqdm
+pip install --upgrade pip
+apt install toilet -y
+pkg install curl
+pkg install wget
+apt install figlet -y
+pkg install git -y
+apt install unzip -y
+gem install lolcat
+toilet -F gay 3-In-1 | lolcat
+sleep 3
+echo""
+echo
+echo -e "\e[36m Now Run 👉 python2 3O1.py"
+echo -e "\e[1m\e[32m Creation \e[31m By \e[33m MarMu (Myanmar)"
+echo
+sleep 2.0
+exit
